b89d1539b12b0c4626dc57fe38b9153249e6f10d
elfketchup <elfketchup@gmail.com>
2013-09-18 09:49:13 -0700

    Initial Commit

---
+++ EKVN test/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  EKVN test
+//
+//  Created by James on 9/18/13.
+//  Copyright James Briones 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
